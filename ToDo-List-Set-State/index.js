25a289f4e1273eab5a0eae92beb65c7ee23823b1
Jade Abel <jade.marie.abel@gmail.com>
2021-05-30 16:01:03 -0400

    onSubmit and handleSubmit events

--- ToDo-List-Set-State/index.js
+++ ToDo-List-Set-State/index.js
@@ -14,16 +14,34 @@ function App() {
       isCompleted: false,
     },
   ]);
-
+  const [value, setValue] = React.useState(" "); //manage variable, i.e., in state, initatlized as empty string
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    const newTodos = [...todos, { text: value, isCompleted: false }];
+    setTodos(newTodos);
+    setValue("");
+  };
   return (
     //return loops through todos and creates div tag
     //key of div is index value and text that is set to that text
+    //write our form with input element where user can input that value
+    //when form is submitted, we handle with 'handleSubmit' (see funciton above)
+    //inside of handleSubmit, we prevent the default which is to reload the page
+    //we have a placeholder "Add Todo" within the onSubmit form
+    //handleSubmit function
     <>
       {todos.map((todo, i) => (
         <div className="todo" key={i}>
           {todo.text}
         </div>
       ))}
+      <form onSubmit={handleSubmit}>
+        <input>
+          type="text" className="input" value={value} placeholder="Add Todo..."
+          onChange={(e) => setValue(e.target.value)}
+        </input>
+      </form>
     </>
   );
 }
