fc6e2fa92b1a94c7c88c69106f14cdedccb2aab2
Jade Abel <jade.marie.abel@gmail.com>
2021-05-29 20:41:17 -0400

    added comments

--- ToDo-List-Set-State/index.js
+++ ToDo-List-Set-State/index.js
@@ -1,8 +1,9 @@
 function App() {
+  //component in which initial state is created
   const [todos, setTodos] = React.useState([
     {
       text: "learn react",
-      isCompleted: false,
+      isCompleted: false, //initiatlized as false
     },
     {
       text: "meet friend for lunch",
@@ -15,6 +16,8 @@ function App() {
   ]);
 
   return (
+    //return loops through todos and creates div tag
+    //key of div is index value and text that is set to that text
     <>
       {todos.map((todo, i) => (
         <div key={i}>{todo.text}</div>
